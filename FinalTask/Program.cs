7669e2bb6f160694cf078005afe2450b19efae1b
Артем <ckripa2013@gmail.com>
2023-03-15 11:40:30 +0300

    One commit

---
+++ FinalTask/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] M;
+Console.WriteLine("Введите массив строк через запятую, без пробелов:");
+string stroka = Console.ReadLine();
+Console.WriteLine("Массив из строк, длинна которых 3 или меньше символа:");
+M = stroka.Split(',');
+var result = new string[M .Length];
+var realSize = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
